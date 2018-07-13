72b7e9ca18a5272b1e683e5f2544f1f94c381c31
Ayush <ayush3298@yahoo.com>
2018-07-13 16:59:17 +0530

    most frequent word

---
+++ most_frequent_word.py
@@ -0,0 +1,14 @@
+def most_freq_word_in_array(str_arr):
+    str_arr.sort()
+    s2 = []
+    for i in str_arr:
+        s2.append(str_arr.count(i))
+    return str_arr[s2.index(max(s2))]
+
+
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        str_arr = [i for i in input().split()][0:n]
+        print(most_freq_word_in_array(str_arr))
